9c7e873f41e3437bc62230e6c2508f1cd9b2b620
Jason Witt <jawittdesigns@gmail.com>
2017-05-24 14:48:45 -0400

    Added array check

--- includes/class-bulk-actions.php
+++ includes/class-bulk-actions.php
@@ -70,7 +70,7 @@ class PDT_Bulk_Actions {
 	public function init() {
 
 		// Set the properties.
-		$this->required_plugins = pro_dev_tools()->get_required_plugins;
+		$this->required_plugins = ( pro_dev_tools()->get_required_plugins ) ? pro_dev_tools()->get_required_plugins : array();
 
 		// Run the hooks.
 		if ( is_allowed_role() ) {
@@ -93,7 +93,7 @@ class PDT_Bulk_Actions {
 
 		$actions['required'] = apply_filters( 'pro_dev_tools_bulk_action_required_text', __( 'Required', 'pro-dev-tools' ) );
 
-		if ( count( $this->required_plugins ) > 0 ) {
+		if ( count( $this->required_plugins ) > 0 && is_array( $this->required_plugins ) ) {
 			$actions['not-required'] = apply_filters( 'pro_dev_tools_bulk_action_not_required_text', __( 'Not Required', 'pro-dev-tools' ) );
 		}
 
